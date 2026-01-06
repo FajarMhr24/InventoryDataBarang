e8b972ef2bbcd7f297b0361dde772c561266084d
Shironeko <fajarmaher256@gmail.com>
2026-01-07 02:04:27 +0700

    Add Database class for database connection

---
+++ code/uas/app/config/database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db_name = "db_inventory";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
